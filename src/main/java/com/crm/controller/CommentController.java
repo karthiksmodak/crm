c00ae4dc00367b7ac1040df05635a53a05ee36df
karthiksmodak <108127209+karthiksmodak@users.noreply.github.com>
2024-11-27 10:38:18 +0530

    Update CommentController.java

--- src/main/java/com/crm/controller/CommentController.java
+++ src/main/java/com/crm/controller/CommentController.java
@@ -21,9 +21,11 @@ public class CommentController {
     public String createComment(@RequestBody Comment comment,
                                 @RequestParam long postId){
         System.out.println(1000);
+        System.out.println(1000);
         Post post = postRepository.findById(postId).get();
         comment.setPost(post);
         commentRepository.save(comment);
+        System.out.println(5000);
         System.out.println(5000);
         return "Saved to Comments";
     }
